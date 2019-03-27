a3c5046e3357873f103353323842a3fd476f2c2f
sravan.charlakola <sravan.charlakola@OIT-CT1ALTSCHAR.som.w2k.state.me.us>
2019-03-27 16:41:48 -0400

    Initial Commit

---
+++ Cricket-Fantasy/src/main/java/com/cricket/fantasy/CricketFantasyApplication.java
@@ -0,0 +1,15 @@
+package com.cricket.fantasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class CricketFantasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricketFantasyApplication.class, args);
+	}
+
+}
