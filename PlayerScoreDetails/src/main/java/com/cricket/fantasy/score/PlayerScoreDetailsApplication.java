3f696bdb197bc7923899f0ec731190ace38d13ac
sravan.charlakola <sravan.charlakola@OIT-CT1ALTSCHAR.som.w2k.state.me.us>
2019-03-28 15:33:46 -0400

    Adding Player Score Details

---
+++ PlayerScoreDetails/src/main/java/com/cricket/fantasy/score/PlayerScoreDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.cricket.fantasy.score;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerScoreDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayerScoreDetailsApplication.class, args);
+	}
+
+}
---
+++ PlayerScoreDetails/src/test/java/com/cricket/fantasy/score/PlayerScoreDetailsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cricket.fantasy.score;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PlayerScoreDetailsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
