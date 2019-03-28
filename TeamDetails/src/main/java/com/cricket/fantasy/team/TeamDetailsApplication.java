feb52e083a918cad46a23e0a6fb37cf36808b7c6
sravan.charlakola <sravan.charlakola@OIT-CT1ALTSCHAR.som.w2k.state.me.us>
2019-03-28 15:35:07 -0400

    Adding Team Details

---
+++ TeamDetails/src/main/java/com/cricket/fantasy/team/TeamDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.cricket.fantasy.team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamDetailsApplication.class, args);
+	}
+
+}
