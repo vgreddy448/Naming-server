f1081521b9aa0ce9e06bb0b45d9432e546ea306f
sravan.charlakola <sravan.charlakola@OIT-CT1ALTSCHAR.som.w2k.state.me.us>
2019-03-28 15:32:09 -0400

    Adding Player Details

---
+++ PlayerDetails/src/main/java/com/cricket/fantasy/players/Players.java
@@ -0,0 +1,84 @@
+package com.cricket.fantasy.players;
+
+import java.sql.Date;
+
+public class Players {
+
+	int playerId;
+	String playerTeam;
+	String playerName;
+	String playerCountry;
+	Date playerDOB;
+	String playerRole;
+	String playerBattingStyle;
+	String playerBowlingStyle;
+	String playerHeight;
+	String playerAuctionPrice;
+	
+	
+	public int getPlayerId() {
+		return this.playerId;
+	}
+	public void setPlayerId(int playerId) {
+		this.playerId = playerId;
+	}
+	public String getPlayerTeam() {
+		return this.playerTeam;
+	}
+	public void setPlayerTeam(String playerTeam) {
+		this.playerTeam = playerTeam;
+	}
+	public String getPlayerName() {
+		return this.playerName;
+	}
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
+	public String getPlayerCountry() {
+		return this.playerCountry;
+	}
+	public void setPlayerCountry(String playerCountry) {
+		this.playerCountry = playerCountry;
+	}
+	public Date getPlayerDOB() {
+		return this.playerDOB;
+	}
+	public void setPlayerDOB(Date playerDOB) {
+		this.playerDOB = playerDOB;
+	}
+	public String getPlayerRole() {
+		return this.playerRole;
+	}
+	public void setPlayerRole(String playerRole) {
+		this.playerRole = playerRole;
+	}
+	public String getPlayerBattingStyle() {
+		return this.playerBattingStyle;
+	}
+	public void setPlayerBattingStyle(String playerBattingStyle) {
+		this.playerBattingStyle = playerBattingStyle;
+	}
+	public String getPlayerBowlingStyle() {
+		return this.playerBowlingStyle;
+	}
+	public void setPlayerBowlingStyle(String playerBowlingStyle) {
+		this.playerBowlingStyle = playerBowlingStyle;
+	}
+	public String getPlayerHeight() {
+		return this.playerHeight;
+	}
+	public void setPlayerHeight(String playerHeight) {
+		this.playerHeight = playerHeight;
+	}
+	public String getPlayerAuctionPrice() {
+		return this.playerAuctionPrice;
+	}
+	public void setPlayerAuctionPrice(String playerAuctionPrice) {
+		this.playerAuctionPrice = playerAuctionPrice;
+	}
+	
+	
+	
+	
+	
+}
