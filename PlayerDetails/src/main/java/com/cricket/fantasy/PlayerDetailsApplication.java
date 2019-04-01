742afe93b33fa4055d66f02d82db759179acace0
sravancharlakola <sravanreddy.charl@gmail.com>
2019-03-31 21:56:14 -0400

    Updated Player Details

--- PlayerDetails/src/main/java/com/cricket/fantasy/PlayerDetailsApplication.java
+++ PlayerDetails/src/main/java/com/cricket/fantasy/PlayerDetailsApplication.java
@@ -2,12 +2,20 @@ package com.cricket.fantasy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PlayerDetailsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlayerDetailsApplication.class, args);
 	}
+	
+	/*@Bean
+	public Sampler defaultSampler(){
+		return Sampler.ALWAYS_SAMPLE;
+	}*/
 
 }
