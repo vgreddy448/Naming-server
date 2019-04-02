880a2d485c70b828bf22a260ca85f98514462e9a
sravan.charlakola <sravan.charlakola@OIT-CT1ALTSCHAR.som.w2k.state.me.us>
2019-04-02 17:18:40 -0400

    Updates to Proxy

--- TeamDetails/src/main/java/com/cricket/fantasy/team/TeamDetailsController.java
+++ TeamDetails/src/main/java/com/cricket/fantasy/team/TeamDetailsController.java
@@ -1,5 +1,31 @@
 package com.cricket.fantasy.team;
 
+import java.math.BigDecimal;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
 public class TeamDetailsController {
+
+private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	TeamDetailsServiceProxy proxy;
+	
+	@GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
+	public TeamDetails convertCurrency(@PathVariable String from, @PathVariable String to,
+			@PathVariable BigDecimal quantity) {
+		
+	}
+	
+	@GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
+	public TeamDetails convertCurrency(@PathVariable String from, @PathVariable String to,
+			@PathVariable BigDecimal quantity) {
+		
+	}
 
 }
