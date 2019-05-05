dd9f96f610f126f536684cad01e0ae3368976af7
sravancharlakola <sravancharlakola@192.168.0.21>
2019-05-04 23:18:51 -0400

    Formatting

--- PlayerDetails/src/main/java/com/cricket/fantasy/rabbitMQ/RabbitConfiguration.java
+++ PlayerDetails/src/main/java/com/cricket/fantasy/rabbitMQ/RabbitConfiguration.java
@@ -45,35 +45,39 @@ import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 
 @Configuration
 public class RabbitConfiguration {
- private static final String LISTENER_METHOD = "receiveMessage";
- @Value("${queue.name}")
- private String queueName;
- @Value("${fanout.exchange}")
- private String fanoutExchange;
- @Bean
- Queue queue() {
-  return new Queue(queueName, true);
- }
- @Bean
- FanoutExchange exchange() {
-  return new FanoutExchange(fanoutExchange);
- }
- @Bean
- Binding binding(Queue queue, FanoutExchange exchange) {
-  return BindingBuilder.bind(queue).to(exchange);
- }
- @Bean
- SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
-  MessageListenerAdapter listenerAdapter) {
-  SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-  container.setConnectionFactory(connectionFactory);
-  container.setQueueNames(queueName);
-  container.setMessageListener(listenerAdapter);
-  return container;
- }
- @Bean
- MessageListenerAdapter listenerAdapter(QueueConsumer consumer) {
-  return new MessageListenerAdapter(consumer, LISTENER_METHOD);
- }
-}
+	private static final String LISTENER_METHOD = "receiveMessage";
+	@Value("${queue.name}")
+	private String queueName;
+	@Value("${fanout.exchange}")
+	private String fanoutExchange;
 
+	@Bean
+	Queue queue() {
+		return new Queue(queueName, true);
+	}
+
+	@Bean
+	FanoutExchange exchange() {
+		return new FanoutExchange(fanoutExchange);
+	}
+
+	@Bean
+	Binding binding(Queue queue, FanoutExchange exchange) {
+		return BindingBuilder.bind(queue).to(exchange);
+	}
+
+	@Bean
+	SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+			MessageListenerAdapter listenerAdapter) {
+		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+		container.setConnectionFactory(connectionFactory);
+		container.setQueueNames(queueName);
+		container.setMessageListener(listenerAdapter);
+		return container;
+	}
+
+	@Bean
+	MessageListenerAdapter listenerAdapter(QueueConsumer consumer) {
+		return new MessageListenerAdapter(consumer, LISTENER_METHOD);
+	}
+}
