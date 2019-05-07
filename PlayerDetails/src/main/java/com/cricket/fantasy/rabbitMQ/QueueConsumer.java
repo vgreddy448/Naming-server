d309ee12070535800f7e1bafcad23b558a5ac2e9
sravancharlakola <sravancharlakola@192.168.0.21>
2019-05-06 21:25:48 -0400

    Moving to MQ branch

--- PlayerDetails/src/main/java/com/cricket/fantasy/rabbitMQ/QueueConsumer.java
+++ PlayerDetails/src/main/java/com/cricket/fantasy/rabbitMQ/QueueConsumer.java
@@ -1,4 +1,4 @@
-/*package com.cricket.fantasy.rabbitMQ;
+package com.cricket.fantasy.rabbitMQ;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -44,4 +44,4 @@ public class QueueConsumer {
 		}
 	}
 
-}*/
+}
